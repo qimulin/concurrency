e7799077c29113fa886edcebaee183fc436f7840
linxc <582192258@qq.com>
2022-09-01 13:53:12 +0800

    原子整数，使用示例

---
+++ src/main/java/lin/xi/chun/concurrency/juc/atomic/integer/AtomicIntegerTest.java
@@ -0,0 +1,48 @@
+package lin.xi.chun.concurrency.juc.atomic.integer;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author zhou.wu
+ * @description: 原子整数测试
+ * @date 2022/9/1
+ **/
+public class AtomicIntegerTest {
+    public static void main(String[] args) {
+        AtomicInteger i = new AtomicInteger(0);
+        // compareAndSet方法不是很方便，往往要配合while循环，以下很多方法，原理也是compareAndSet方法配合while循环
+
+        // 获取并自增（i = 0, 结果 i = 1, 返回 0），类似于 i++
+        System.out.println(i.getAndIncrement());
+        // 自增并获取（i = 1, 结果 i = 2, 返回 2），类似于 ++i
+        System.out.println(i.incrementAndGet());
+
+        // 自减并获取（i = 2, 结果 i = 1, 返回 1），类似于 --i
+        System.out.println(i.decrementAndGet());
+        // 获取并自减（i = 1, 结果 i = 0, 返回 1），类似于 i--
+        System.out.println(i.getAndDecrement());
+
+        // 获取并加值（i = 0, 结果 i = 5, 返回 0）
+        System.out.println(i.getAndAdd(5));
+        // 加值并获取（i = 5, 结果 i = 0, 返回 0）
+        System.out.println(i.addAndGet(-5));
+
+        // getAndUpdate 可以用作一些非加减的运算，例如乘法运算。接收的是一个接口类型@FunctionalInterface，配合lambda使用
+        // 获取并更新（i = 0, p 为 i 的当前值, 结果 i = -2, 返回 0）
+        // 其中函数中的操作能保证原子，但函数需要无副作用
+        System.out.println(i.getAndUpdate(p -> p - 2));
+        // 更新并获取（i = -2, p 为 i 的当前值, 结果 i = 0, 返回 0）
+        // 其中函数中的操作能保证原子，但函数需要无副作用
+        System.out.println(i.updateAndGet(p -> p + 2));
+
+        // 可以指定x参数配合得到新值，也可以指定它们的运算方法
+        // 获取并计算（i = 0, p 为 i 的当前值, x 为参数1, 结果 i = 10, 返回 0）
+        // 其中函数中的操作能保证原子，但函数需要无副作用
+        // getAndUpdate 如果在 lambda 中引用了外部的局部变量，要保证该局部变量是 final 的
+        // getAndAccumulate 可以通过 参数1 来引用外部的局部变量，但因为其不在 lambda 中因此不必是 final
+        System.out.println(i.getAndAccumulate(10, (p, x) -> p + x));
+        // 计算并获取（i = 10, p 为 i 的当前值, x 为参数1, 结果 i = 0, 返回 0）
+        // 其中函数中的操作能保证原子，但函数需要无副作用
+        System.out.println(i.accumulateAndGet(-10, (p, x) -> p + x));
+    }
+}
